edffa98f6d9ec2685b85b7cf42d75d3ae5c7a22a
nimeshkk <nimeshkliyanage@gmail.com>
2023-10-09 20:09:45 +0530

    update

--- Library_Management/Library_Management/Dashbord.cs
+++ Library_Management/Library_Management/Dashbord.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Library_Management
 {
-    public partial class Form1 : Form
+    public partial class Dashbord : Form
     {
-        public Form1()
+        public Dashbord()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
