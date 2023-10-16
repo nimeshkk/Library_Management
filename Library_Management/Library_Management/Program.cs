3e97209d08f316ee61f5aea38f2070f9504a35ea
nimeshkk <nimeshkliyanage@gmail.com>
2023-10-17 01:33:58 +0530

    Complete

--- Library_Management/Library_Management/Program.cs
+++ Library_Management/Library_Management/Program.cs
@@ -16,7 +16,7 @@ namespace Library_Management
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Dashbord());
+            Application.Run(new Login());
         }
     }
 }
